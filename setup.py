578accbaa0643b5768841254e97c5c7049a2977f
Jin Zhe <thejinzhe@gmail.com>
2021-07-30 19:09:12 +0800

    reorganize package structure

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name='jinlib',
@@ -7,7 +7,11 @@ setup(
   url='git@github.com:jin-zhe/jinlib.git',
   author='JIN Zhe',
   author_email='thejinzhe@gmail.com',
+  classifiers=[
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+  ],
   license='unlicense',
-  packages=['.'],
+  packages=find_packages(),
   zip_safe=False
 )
