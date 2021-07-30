ccb04594598a778bd0c225b87208e296e4672178
Jin Zhe <thejinzhe@gmail.com>
2021-07-30 18:51:47 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='jinlib',
+  version='0.0.1',
+  description='Personal convenience library',
+  url='git@github.com:jin-zhe/jinlib.git',
+  author='JIN Zhe',
+  author_email='thejinzhe@gmail.com',
+  license='unlicense',
+  packages=['jinlib'],
+  zip_safe=False
+)
