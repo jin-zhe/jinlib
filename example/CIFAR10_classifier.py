2a402da3ce7bb1e08d4e4669585a79998801ea3c
Jin Zhe <thejinzhe@gmail.com>
2021-08-05 02:30:04 +0800

    add analyze code

--- example/CIFAR10_classifier.py
+++ example/CIFAR10_classifier.py
@@ -1,3 +1,4 @@
+from jinlib.visualisation import plot_classwise_accuracies, plot_confusion_matrix
 from jinlib import Experiment
 
 from torch.utils.data import DataLoader
@@ -57,9 +58,31 @@ class CIFAR10Classifier(Experiment):
       self.dataset.train, batch_size=self.batch_size, shuffle=False, num_workers=2)
     self.validation_loader = DataLoader(
       self.dataset.validation, batch_size=self.batch_size, shuffle=False, num_workers=2)
+    self.test_loader = self.analyze_loader = self.validation_loader
+
+  def _init_iter_stats(self):
+    super()._init_iter_stats()
+    if self.context == 'analyze':
+      self.curr_iter_stats['confusion_matrix'] = torch.zeros(len(self.classes), len(self.classes), dtype=torch.float64)
+
+  def _update_iter_stats(self, T_out, T_x):
+    super()._update_iter_stats(T_out, T_x)
+    if self.context == 'analyze':
+      _, T_predictions = torch.max(T_out, 1)
+      for true_label, pred_label in zip(T_x.view(-1), T_predictions.view(-1)):
+        self.curr_iter_stats['confusion_matrix'][true_label.long()][pred_label.long()] += 1
+
+  def analyze(self):
+    super().analyze()
+    confusion_matrix = self.curr_iter_stats['confusion_matrix']
+    confusion_matrix = confusion_matrix / confusion_matrix.sum(axis=1) # normalize
+    classwise_accuracies = confusion_matrix.diag()
+    plot_confusion_matrix(self.classes, confusion_matrix, self.experiment_dir)
+    plot_classwise_accuracies(self.classes, classwise_accuracies, self.experiment_dir)
 
 def main():
   exp1 = CIFAR10Classifier(Path('experiment_1'))
   exp1.train()
+  exp1.analyze()
 
 if __name__ == '__main__': main()
