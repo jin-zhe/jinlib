8446c125356d415c2171bef0dcfcf40c60ebe2b0
Jin Zhe <thejinzhe@gmail.com>
2021-08-18 02:51:38 +0800

    generalize confusion matrix plots for up to 64x64 matrix

--- jinlib/visualisation.py
+++ jinlib/visualisation.py
@@ -6,37 +6,56 @@ from pathlib import Path
 
 from .general import save_pickle
 
-def plot_confusion_matrix(classnames, confusion_matrix, experiment_dir: Path,
-  figsize=None, annot=True, cmap='Greens', dpi=300, filename='confusion_matrix.png', save_data=True):
+def propose_dpi(smallset_fontsize):
+  '''
+  Propose reasonable DPI based on smallest fontsize in the plot
+  '''
+  return -500 * smallset_fontsize/10 + 500
+
+def plot_confusion_matrix(classes, confusion_matrix, experiment_dir: Path, annot_size=None, dpi=None, figsize=None, annot=True, cmap='Greens', filename='confusion_matrix.png', save_data=True):
+  '''
+  Plot a confusion matrix
+  '''
   outpath = experiment_dir / filename
   if save_data:
-    save_pickle({'classnames': classnames, 'confusion_matrix': confusion_matrix}, outpath.with_suffix('.pkl'))
+    save_pickle({'classes': classes, 'confusion_matrix': confusion_matrix}, outpath.with_suffix('.pkl'))
+  
   if figsize:
     plt.figure(figsize=figsize)
   else:
     plt.figure()
-  hm = sns.heatmap(confusion_matrix*100, vmin=0, vmax=100, annot=annot,
-    cmap=cmap, square=True, fmt='.1f')
-  hm.set_xticks(list(range(len(classnames))))   # see issue https://github.com/matplotlib/matplotlib/issues/17736/
-  hm.set_xticklabels(classnames, rotation=-90)
-  hm.set_yticks(list(range(len(classnames))))   # see issue https://github.com/matplotlib/matplotlib/issues/17736/
-  hm.set_yticklabels(classnames, rotation=0)
-  hm.set_title('Confusion matrix (%)')
+  
+  if annot_size is None:
+    annot_size = -0.1 * len(classes) + 8.15
+  tick_size = annot_size * 1.5
+
+  if dpi is None:
+    dpi = propose_dpi(annot_size)
+  
+  print('Annotation size: {}, tick size: {}, DPI: {}'.format(annot_size, tick_size, dpi))
+  data = pd.DataFrame(confusion_matrix*100, columns=classes, index=classes)
+  hm = sns.heatmap(data, vmin=0, vmax=100, xticklabels=1, yticklabels=1, annot=annot, cmap=cmap, square=True, fmt='.1f', annot_kws={'fontsize': annot_size})
+  plt.setp(hm.get_xticklabels(), fontsize=tick_size)
+  plt.setp(hm.get_yticklabels(), fontsize=tick_size)
+  plt.title('Confusion matrix (%)')
+  plt.xlabel('Predicted')
+  plt.ylabel('Ground Truth')
+  plt.tight_layout()
   plt.savefig(str(outpath.resolve()), dpi=dpi)
   plt.close()
   print(f'Confusion matrix plot saved to {str(outpath.resolve())}')
 
-def plot_classwise_accuracies(classnames, classwise_accuracies, experiment_dir: Path,
+def plot_classwise_accuracies(classes, classwise_accuracies, experiment_dir: Path,
   figsize=None, color='b', dpi=300, filename='classwise_accuracies.png', save_data=True):
   outpath = experiment_dir / filename
   if save_data:
-    save_pickle({'classnames': classnames, 'classwise_accuracies': classwise_accuracies}, outpath.with_suffix('.pkl'))
+    save_pickle({'classes': classes, 'classwise_accuracies': classwise_accuracies}, outpath.with_suffix('.pkl'))
   if figsize:
     plt.figure(figsize=figsize)
   else:
     plt.figure()
   df = pd.DataFrame((classwise_accuracies*100), columns=['Accuracy'])
-  df['Label'] = classnames
+  df['Label'] = classes
   bp = sns.barplot(data=df, x='Accuracy', y='Label', order=df.sort_values('Accuracy').Label, edgecolor='w')
   for bar in bp.patches:
     width = bar.get_width()
